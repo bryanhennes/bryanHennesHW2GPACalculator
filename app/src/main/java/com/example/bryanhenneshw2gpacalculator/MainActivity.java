38492dd0a1953a3647dc13c88058dd2740b4ead0
Bryan Hennes <Bryanhennes@gmail.com>
2021-09-19 10:25:35 -0400

    Initial commit

---
+++ app/src/main/java/com/example/bryanhenneshw2gpacalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bryanhenneshw2gpacalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
